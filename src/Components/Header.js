2fbd4932d5ee5b87629b9cad5f85456a09776dca
santiectro <santiago.barahona67@gmail.com>
2023-02-03 21:44:46 -0500

    fisrt commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Header () {
+    return (
+        <nav>
+            <div>
+                <img src="../Images/me-profile.jpeg" />
+                <p>David Barahona</p>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
