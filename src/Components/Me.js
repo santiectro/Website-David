49e154dd72adb7293d096071208621bfad7f5044
santiectro <santiago.barahona67@gmail.com>
2023-02-05 02:53:21 -0500

    Change about me section

--- src/Components/Me.js
+++ src/Components/Me.js
@@ -15,22 +15,12 @@ export default function Me () {
                     <img src={Jane} alt="Jane smiling" />
                 <div>
                     <p>
-                        Est cupidatat consequat ea ea eiusmod Lorem esse irure et ullamco est laborum reprehenderit. 
-                        Ea consequat nisi irure nisi officia quis laboris consequat proident. Excepteur irure cillum 
-                        sit eiusmod velit labore et. Deserunt laboris ipsum aliqua cupidatat ex mollit veniam aliqua 
-                        enim cupidatat nostrud dolor tempor. Fugiat dolor non dolore sint. Anim fugiat dolore sit et 
-                        ex incididunt anim qui culpa sint velit.
+                        My name is David Barahona, I'm from Colombia currently living in Canada 
+                        I have various hobbies, like playing soccer, stocks, videogames and coding.
+                        As we speak I'm 22 years old I started coding in 2021 after watching a video
+                        about a guy coding some challenges in Ruby, never stopped since, I also consider
+                        myself a music lover and as a porpuse of 2023 I decided to learn the piano
                     </p>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <p>
-                        Voluptate ut occaecat excepteur reprehenderit fugiat aliqua amet duis cillum non aliquip non nulla aute. 
-                        Proident officia esse voluptate magna pariatur irure do qui laborum exercitation deserunt pariatur minim. 
-                        Laborum adipisicing et ad incididunt. Sint esse nulla consequat irure irure aute pariatur exercitation 
-                        mollit proident. Aliquip aliquip voluptate aute minim esse deserunt tempor consectetur. Officia ea amet 
-                        fugiat elit.
-                    </p>    
                     <button className={`${Lightmode ? "light" : "dark"}`}> Check my resume </button>
                 </div>
             </div>
