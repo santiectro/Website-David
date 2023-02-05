f4099ba4d0bd154f7e0d41ab5bdbef0798d05fb3
santiectro <santiago.barahona67@gmail.com>
2023-02-05 17:27:50 -0500

    Added working link on Contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -18,16 +18,24 @@ export default  function Contact() {
             <div className={`contact  ${ Lightmode ? "light" : "dark"  }`} id="contact-me" >
                 <h3>Contact</h3>
                 <p>
-                    Deserunt excepteur eu sit voluptate cillum proident culpa dolore excepteur labore minim ea minim. 
-                    In enim elit magna elit eu ad amet ea irure duis esse ea id reprehenderit. Mollit officia fugiat 
-                    aliquip sit aute consectetur cillum est consequat ullamco minim aute. Quis aliquip est aliquip 
-                    reprehenderit officia nisi nisi anim ipsum.
+                    If you have any inquiries or would like to contact me please send me and email to 
+                    santiago.barahona67@gmail.com or call me at +1-819-328-1724, you can also reach me 
+                    trough any of my social media platforms down below
                 </p>
                 <ul>
-                    <li><FontAwesomeIcon icon="fa-brands fa-facebook" size="2x" /></li>
-                    <li><FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" /></li>
-                    <li><FontAwesomeIcon icon="fa-brands fa-github"  size="2x"/></li>
-                    <li><FontAwesomeIcon icon="fa-brands fa-linkedin" size="2x" /></li>
+                    <a href="https://twitter.com/santi632">
+                        <li><FontAwesomeIcon icon="fa-brands fa-twitter" size="2x" /></li>
+                    </a>
+                    <a href="https://www.instagram.com/santi_barahona347/">
+                        <li><FontAwesomeIcon icon="fa-brands fa-instagram" size="2x" /></li>
+                    </a>
+                    <a href="https://github.com/santiectro">
+                        <li><FontAwesomeIcon icon="fa-brands fa-github"  size="2x"/></li>
+                    </a>
+                    <a href="https://www.linkedin.com/in/david-barahona-a298a81a0/">
+                        <li><FontAwesomeIcon icon="fa-brands fa-linkedin" size="2x" /></li>
+                    </a>
+                    
                 </ul>
             </div>
         </>
